a9b7f241c670343ab63e3a6e6bb19b2ffba786af
Thomas Strohmeier <tstrohmeier@users.noreply.github.com>
2018-03-28 03:13:29 +0200

    Update entrypoint.sh
    formated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+####
+# Author: Thomas Strohmeier
+####
+
 set -e
 
 while getopts h: option
 do
-case "${option}"
+  case "${option}"
 in
-h) HOST=${OPTARG};;
-
+  h) HOST=${OPTARG};;
 esac
 done
 
 while true
 do
-	echo "Press [CTRL+C] to stop.."
-    curl -s -o /dev/null -w "%{url_effective} [%{http_code}]\n" $HOST
-	sleep 1
+  echo "Press [CTRL+C] to stop.."
+  curl -s -o /dev/null -w "%{url_effective} [%{http_code}]\n" $HOST
+  sleep 1
 done
